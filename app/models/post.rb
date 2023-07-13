319c5d557c76ed85f72ff7998759413a3d53d9d6
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-13 17:11:48 -0400

    Post method to update user counter and get the last 5 comments on Post added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_counter
+    author.increment!(:post_counter)
+  end
+
+  def last_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
