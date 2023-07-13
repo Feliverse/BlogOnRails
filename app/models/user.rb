299dc4d09948f05b9e99dbd121c01f041eb1e3c3
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-13 17:08:16 -0400

    User method to list the 3 last posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def last_3_post
+    posts.order(created_at: :desc).limit(3)
+  end
 end
