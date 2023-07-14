b58c2df24f5eaa8f94dcf8958f37508b2303ace4
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-14 14:13:17 -0400

    Testing Comment Model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '#update_counter' do
+    author = User.create!(name: 'test', post_counter: 0)
+    post = Post.create!(title: 'first post', comments_counter: 1, likes_counter: 1, author: author)
+    Comment.create!(post: post, author: author)
+    expect(post.comments_counter).to eq(2)
+  end
 end
