821d806d65f234e1fcd2b9693b0468db97f00217
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-14 09:06:37 -0400

    Rubocop offenses fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,14 @@ class Post < ApplicationRecord
   has_many :likes
 
   after_save :update_counter
-  
+
   def last_5_comments
     comments.order(created_at: :desc).limit(5)
   end
-  
+
   private
 
   def update_counter
     author.increment!(:post_counter)
   end
-  
 end
