30b9f85aeb6d650283a34afddd6b00ea487daac6
Felipe Haybar Valenzuela <114632980+Feliverse@users.noreply.github.com>
2023-07-14 08:03:44 -0400

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
-end
 
-after_save :update_counter
+  after_save :update_counter
 
-private
+  private
 
-def update_counter
-  post.increment!(:comments_counter)
+  def update_counter
+    post.increment!(:comments_counter)
+  end
 end
