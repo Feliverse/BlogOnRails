8a9a8d2a3d5dfea18fe0f830c15f7491cd6fb840
Felipe Haybar Valenzuela <114632980+Feliverse@users.noreply.github.com>
2023-07-14 08:00:44 -0400

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,15 @@ class Post < ApplicationRecord
   has_many :likes
 
   after_save :update_counter
-
+  
+  def last_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+  
   private
 
   def update_counter
     author.increment!(:post_counter)
   end
-
-  def last_5_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+  
 end
