f2e181a9870414166f48010ff59d5070f8638065
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-14 11:59:10 -0400

    Validations in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
+  
   def last_3_post
     posts.order(created_at: :desc).limit(3)
   end
