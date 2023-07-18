e57a3d643a7aca2b807bdc1db860851c50544b5d
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-18 07:41:45 -0400

    Config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users, only: [:index, :show] do
