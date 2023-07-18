b3afced32f3356b58645ddc7073a7b29cf344aed
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-18 07:22:27 -0400

    Create Routes for each URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  resourses :users, only: [:index, :show] do
+    resourses :posts, only: [:index, :show]
+  end
 end
