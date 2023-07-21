1412f1dac78618cb7d9200a039c563ab38f9bd3d
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-20 22:23:32 -0400

    Config controller to like posts

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.new(like_params)
+    @like.author_id = current_user.id
+    @like.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @like = Like.find_by(like_params)
+    @like.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:post_id :author_id)
+  end
+end
