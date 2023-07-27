69d46ad252358730c8b62d570b476a27a85eb895
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-27 09:31:26 -0400

    rails generate devise:controllers scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users, only: [:index, :show] do
