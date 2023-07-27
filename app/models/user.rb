21133f70c80759e63dbd16828366e21c5614222d
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-07-27 09:23:57 -0400

    rails generate devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   attribute :post_counter, default: 0
 
   has_many :posts, foreign_key: 'author_id'
